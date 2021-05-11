1c614c67167f77a618d510a3f5f7b779cb070c7b
実方優 <jitsukatayuu@jitsukatayuunoMacBook-Air.local>
2021-05-11 20:06:38 +0900

    タスク８修正

--- src/main/java/jp/co/seattle/library/controller/BulkRegistrationController.java
+++ src/main/java/jp/co/seattle/library/controller/BulkRegistrationController.java
@@ -37,7 +37,10 @@ public class BulkRegistrationController {
     @Autowired
     private BooksService booksService;
 
-    // 入力された値を取得し、"bulkRegistration"に渡す
+    /**
+     *  入力された値を取得し、bulkRegistrationに渡す
+     * 
+     */
     @RequestMapping(value = "/bulkRegistration", method = RequestMethod.GET) //value＝actionで指定したパラメータ
 
     //RequestParamでname属性を取得
@@ -45,7 +48,10 @@ public class BulkRegistrationController {
         return "bulkRegistration";
     }
 
-        
+    /**
+     *  書籍情報を一括登録する
+     * @param upload_file
+     */
     @Transactional
     @RequestMapping(value = "/bulkRegistrationBook", method = RequestMethod.POST, produces = "text/plain;charset=utf-8")
     public String insertBook(Locale locale,
@@ -58,90 +64,92 @@ public class BulkRegistrationController {
 
         //エラーメッセージのリスト生成
         List<String> errorMessages = new ArrayList<String>();
-        
+
         // 複数の本の情報を入れるList生成。STrign型の配列が入力される場合だから<String[]>となる
         List<String[]> list = new ArrayList<String[]>();
-        
+
         //String型のlineさんを中身はnullで生成
         String line;
 
         boolean flag = false;
 
         try {
-         // データの読み書きを行う。連続したバイトとして読み込む。
-            InputStream InStream = multipartFile.getInputStream(); 
+            // データの読み書きを行う。連続したバイトとして読み込む。
+            InputStream InStream = multipartFile.getInputStream();
 
             Reader reader = new InputStreamReader(InStream);
-            
+
             // ストリームをバッファする
-            BufferedReader buf = new BufferedReader(reader); 
-            
-            int a = 0;
+            BufferedReader buf = new BufferedReader(reader);
 
-
+            int number = 0;
 
             //１行分のデータがある場合は行を読み込むよ
             while ((line = buf.readLine()) != null) {
                 // カンマでlineを分割し、配列に格納する
                 String[] book = line.split(",");
-                a++;
+                number++;
                 list.add(book);
 
                 if (multipartFile.isEmpty()) {
                     errorMessages.add("値が入っていません。ファイルの中身を確認してください");
                     return "bulkRegistration";
                 }
+                // 必須項目のチェック
+                if (book[0].isEmpty() || book[1].isEmpty() || book[2].isEmpty() || book[3].isEmpty()) {
+                    errorMessages.add(number + "行目の必須項目を確認してください");
+                    flag = true;
+                }
 
-            //出版日のバリデーションチェック
-            try {
-            SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
-            df.setLenient(false);
-            df.parse(book[3]);
-            } catch (ParseException e) {
-                errorMessages.add(a + "行目の出版日はyyyyMMdd形式で入力してください");
-            flag = true;
-        }
-            // ISBNの文字形式のバリデーションチェック
-            if ((book[4] != null || book[4].isEmpty()) && !book[4].matches("[0-9]{10}|[0-9]{13}")) {
-                errorMessages.add(a + "行目のISBNは10桁または13桁の半角数字で入力してください");
-            flag = true;
+                //出版日のバリデーションチェック
+                try {
+                    SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+                    df.setLenient(false);
+                    df.parse(book[3]);
+                } catch (ParseException e) {
+                    errorMessages.add(number + "行目の出版日はyyyyMMdd形式で入力してください");
+                    flag = true;
+                }
+                // ISBNの文字形式のバリデーションチェック
+                if ((book[4] != null || book[4].isEmpty()) && !book[4].matches("[0-9]{10}|[0-9]{13}")) {
+                    errorMessages.add(number + "行目のISBNは10桁または13桁の半角数字で入力してください");
+                    flag = true;
+                }
             }
-        }
 
-        // エラーが一回でもなったら
-        if (flag) {
-            //エラーだよ
-            model.addAttribute("errormessages", errorMessages);
+            // エラーが一回でもなったら
+            if (flag) {
+                //エラーだよ
+                model.addAttribute("errormessages", errorMessages);
+                return "bulkRegistration";
+            }
+            buf.close();
+
+            // 書籍を登録する
+            for (int i = 0; i < list.size(); i++) {
+                BookDetailsInfo bookInfo = new BookDetailsInfo();
+                bookInfo.setTitle(list.get(i)[0]);
+                bookInfo.setAuthor(list.get(i)[1]);
+                bookInfo.setPublisher(list.get(i)[2]);
+                bookInfo.setPublishDate(list.get(i)[3]);
+                bookInfo.setIsbn(list.get(i)[4]);
+                bookInfo.setDescription(list.get(i)[5]);
+                // 書籍情報を新規登録する
+                // booksServiceのregistBookメソッドを呼び出し、登録している
+
+                booksService.registBook(bookInfo);
+            }
+
+            // 書籍情報を登録完了だよ
+            model.addAttribute("registComplete", "登録完了");
+
+            // 一括登録画面に遷移する
+            return "bulkRegistration";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("error", "ファイル読み込み失敗");
             return "bulkRegistration";
         }
-        buf.close();
-      
-        // 書籍を登録する
-        for (int i = 0; i < list.size(); i++) {
-            BookDetailsInfo bookInfo = new BookDetailsInfo();
-            bookInfo.setTitle(list.get(i)[0]);
-            bookInfo.setAuthor(list.get(i)[1]);
-            bookInfo.setPublisher(list.get(i)[2]);
-            bookInfo.setPublishDate(list.get(i)[3]);
-            bookInfo.setIsbn(list.get(i)[4]);
-            bookInfo.setDescription(list.get(i)[5]);
-            // 書籍情報を新規登録する
-            // booksServiceのregistBookメソッドを呼び出し、登録している
-
-            booksService.registBook(bookInfo);
-        }
-
-        // 書籍情報を登録完了だよ
-        model.addAttribute("registComplete", "登録完了");
-        
-        // 一括登録画面に遷移する
-        return "bulkRegistration";
-        
-    } catch (IOException e) {
-        e.printStackTrace();
-        model.addAttribute("error", "ファイル読み込み失敗");
-        return "bulkRegistration";
-       }
     }
 }
-
